9df0b0af2a27f918661de79e47bba1b4a547da6f
EliasLoaiza <eliasloaizarincon@gmail.com>
2022-03-23 00:37:38 -0400

    continuando2

--- src/views/Usuario/IngresarUsuario/index.js
+++ src/views/Usuario/IngresarUsuario/index.js
@@ -15,22 +15,20 @@ import {
   CInputGroupText,
   CRow,
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
 /**
  * @author
  * @function IngresarUsuario
  **/
 const IngresarUsuario = (props) => {
   const [usuario, setUsuario] = useState({
-    cedula: '',
-    nombre: '',
-    apellido: '',
-    correo: '',
-    telefono: 0,
-    tipo: '',
-    direccion: '',
-    fecha_nacimiento: '',
+    "cedula": 1, /* eslint-disable-line*/
+    "nombre": "", /* eslint-disable-line*/
+    "apellido": "", /* eslint-disable-line*/
+    "correo": "", /* eslint-disable-line*/
+    "telefono": 1, /* eslint-disable-line*/
+    "tipo": "", /* eslint-disable-line*/
+    "direccion": "", /* eslint-disable-line*/
+    "fecha_nacimiento": "" /* eslint-disable-line*/
   })
   const handleChange = (e) => {
     setUsuario({
@@ -39,12 +37,28 @@ const IngresarUsuario = (props) => {
     })
   }
   const handleSubmit = () => {
-    let fechaNacimiento = document.getElementById('Dia_Nacimiento').value + "-" + document.getElementById('Mes_Nacimiento')/value + "-" + document.getElementById('Año_Nacimiento')/value /* eslint-disable-line*/
-    console.log(fechaNacimiento)
     if (usuario.nombre === '' || usuario.cedula === '' || usuario.correo === ''){ /* eslint-disable-line*/
       alert('Los campos de Nombre, Cedula y Correo, son obligatorios')
       return
     }
+    const requestInit = {
+      method: 'POST',
+      header: { 'Content Type': 'aplication/json' },
+      body: JSON.stringify(usuario),
+    }
+    fetch('http://localhost:9000/Usuario', requestInit)
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+    setUsuario({
+      "cedula": 1, /* eslint-disable-line*/
+      "nombre": "", /* eslint-disable-line*/
+      "apellido": "", /* eslint-disable-line*/
+      "correo": "", /* eslint-disable-line*/
+      "telefono": 1, /* eslint-disable-line*/
+      "tipo": "", /* eslint-disable-line*/
+      "direccion": "", /* eslint-disable-line*/
+      "fecha_nacimiento": "" /* eslint-disable-line*/
+    })
   }
   return (
     <div className="bg-light min-vh-50 d-flex flex-row align-items-center">
@@ -55,13 +69,16 @@ const IngresarUsuario = (props) => {
               <CCardBody className="p-4">
                 <CForm onSubmit={handleSubmit}>
                   <CInputGroup className="mb-3">
+                    <CFormInput id="Cedula" name="cedula" onChange={handleChange} placeholder="Cedula" type="number" /> {/* eslint-disable-line*/}
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
                     <CFormInput id="Nombre" name="nombre" onChange={handleChange} placeholder="Nombres" type="text" /> {/* eslint-disable-line*/}
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CFormInput id="Apellido" name="apellido" onChange={handleChange} placeholder="Apellidos" type="text" /> {/* eslint-disable-line*/}
                   </CInputGroup>
                   <CInputGroup className="mb-3">
-                    <CFormInput id="Cedula" name="cedula" onChange={handleChange} placeholder="Cedula" type="number" /> {/* eslint-disable-line*/}
+                    <CFormInput id="Correo" name="correo" onChange={handleChange} placeholder="Correo" type="email" /> {/* eslint-disable-line*/}
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CFormInput id="Telefono" name="telefono" onChange={handleChange} placeholder="Telefono" type="number" /> {/* eslint-disable-line*/}
@@ -74,24 +91,21 @@ const IngresarUsuario = (props) => {
                     </CFormSelect>
                   </CInputGroup>
                   <CInputGroup className="mb-3">
-                    <CFormInput id="Correo" name="correo" onChange={handleChange} placeholder="Correo" type="email" /> {/* eslint-disable-line*/}
-                  </CInputGroup>
-                  <CInputGroup className="mb-3">
                     <CFormLabel htmlFor="exampleFormControlTextarea1">Direccion</CFormLabel>
                     <CInputGroup className="mb-3">
                       <CFormTextarea id="Direccion" name="direccion" onChange={handleChange} rows="3"></CFormTextarea> {/* eslint-disable-line*/}
                     </CInputGroup>
                   </CInputGroup>
                   <CInputGroup className="mb-3">
-                    <CFormInput id="Codigo_Cargo" onChange={handleChange} placeholder="Codigo del Cargo" type="text" /> {/* eslint-disable-line*/}
+                    <CFormInput id="Codigo_Cargo" placeholder="Codigo del Cargo" type="text" /> {/* eslint-disable-line*/}
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroup className="mb-3">
                       <CFormLabel htmlFor="">Fecha de Nacimiento</CFormLabel>
                     </CInputGroup>
-                    <CFormInput id="Dia_Nacimiento" name="Dia_Nacimiento" onChange={handleChange} placeholder="Dia" type="number" /> {/* eslint-disable-line*/}
-                    <CFormInput id="Mes_Nacimiento" name="Mes_Nacimiento" onChange={handleChange} placeholder="Mes" type="number" /> {/* eslint-disable-line*/}
-                    <CFormInput id="Año_Nacimiento" name="Año_Nacimiento" onChange={handleChange} placeholder="Años" type="number" /> {/* eslint-disable-line*/}
+                    <CFormInput id="Dia_Nacimiento" name="Dia_Nacimiento" placeholder="Dia" type="number" /> {/* eslint-disable-line*/}
+                    <CFormInput id="Mes_Nacimiento" name="Mes_Nacimiento" placeholder="Mes" type="number" /> {/* eslint-disable-line*/}
+                    <CFormInput id="Año_Nacimiento" name="Año_Nacimiento" placeholder="Años" type="number" /> {/* eslint-disable-line*/}
                   </CInputGroup>
                   <div className="d-grid">
                     <CButton type="submit" color="success">Ingresar Usuario</CButton> {/* eslint-disable-line*/}
